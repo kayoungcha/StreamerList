eb04dc9e84d2580813c8b74a93ffe8a7abe0bfb7
ky <rkdud9941@gmail.com>
2025-03-14 00:17:56 +0900

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+  ],
+};
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDZ2SYYFYbcGk3zx29xzU3xmTB_6OnhYgs',
+    authDomain: 'streamerlist-fc911.firebaseapp.com',
+    projectId: 'streamerlist-fc911',
+    storageBucket: 'streamerlist-fc911.firebasestorage.app',
+    messagingSenderId: '695085530167',
+    appId: '1:695085530167:web:6e068aebe322e9433d53b2',
+  },
+};
